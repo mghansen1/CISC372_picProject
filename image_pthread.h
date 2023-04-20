e70baff901e9051ea43a97cb7ebbc9d150ab8df8
Matt <mghansen@udel.edu>
2023-04-19 23:28:03 -0400

    l

--- image_pthread.h
+++ image_pthread.h
@@ -16,8 +16,8 @@ enum KernelTypes{EDGE=0,SHARPEN=1,BLUR=2,GAUSE_BLUR=3,EMBOSS=4,IDENTITY=5};
 typedef double Matrix[3][3];
 
 struct args_struct{
-    Image* source_image;
-    Image* dest_image;
+    Image* srcimage;
+    Image* destimage;
     enum KernelTypes type;
     long rank;
 };
