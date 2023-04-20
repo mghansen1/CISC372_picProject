ab0e5d135a1d90f9296031b24f11ed922a20fe91
Matt <mghansen@udel.edu>
2023-04-19 23:55:46 -0400

    spelling :(

--- image_pthread.h
+++ image_pthread.h
@@ -15,7 +15,7 @@ enum KernelTypes{EDGE=0,SHARPEN=1,BLUR=2,GAUSE_BLUR=3,EMBOSS=4,IDENTITY=5};
 
 typedef double Matrix[3][3];
 
-struct args_struct{
+struct arg_struct{
     Image* srcImage;
     Image* destImage;
     enum KernelTypes type;
